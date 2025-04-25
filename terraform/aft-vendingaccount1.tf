db0306546e46b2390720f18431b58f0e9765f018
rameshdonti <d.rameshdba@gmail.com>
2025-04-25 21:49:35 +0530

    Create aft-vendingaccount1.tf

---
+++ terraform/aft-vendingaccount1.tf
@@ -0,0 +1,27 @@
+module "aft-vendingaccount1" {
+
+  source = "github.com/rameshdonti/aft-account-request.git"
+
+  control_tower_parameters = {
+    AccountEmail              = "ramdonti+sandbox1@amazon.com"
+    AccountName               = "sandbox-aft"
+    ManagedOrganizationalUnit = "Custom"
+    SSOUserEmail              = "ramdonti@amazon.com"
+    SSOUserFirstName          = "sandbox1"
+    SSOUserLastName           = "SSO"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+}
