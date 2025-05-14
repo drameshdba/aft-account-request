c4992078b3f6faf023c876e4f0fe9389620bc08c
rameshdonti <d.rameshdba@gmail.com>
2025-05-14 14:11:10 +0530

    Update account-request.tf
    removed space in email

--- terraform/account-request.tf
+++ terraform/account-request.tf
@@ -6,19 +6,19 @@ module "nico-account-2" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail = "ramdonti+newaccount@amazon.com "
+    AccountEmail = "ramdonti+newaccount@amazon.com"
     AccountName  = "nico-account-2"
     # Syntax for top-level OU
     ManagedOrganizationalUnit = "Sandbox"
     # Syntax for nested OU
     # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
-    SSOUserEmail     = "ramdonti+newaccount@amazon.com "
+    SSOUserEmail     = "ramdonti+newaccount@amazon.com"
     SSOUserFirstName = "nico"
     SSOUserLastName  = "gagnon"
   }
 
   account_tags = {
-    "ABC:Owner"       = "ramdonti+newaccount@amazon.com "
+    "ABC:Owner"       = "ramdonti+newaccount@amazon.com"
     "ABC:Division"    = "ENT"
     "ABC:Environment" = "Dev"
     "ABC:CostCenter"  = "123456"
