27b0a92c9f3ade8a4afc65cb16715eccdacb5a11
dhruvbakshi316 <dhruvbakshi316@gmail.com>
2025-05-20 08:51:17 -0700

    Update account-request.tf

--- terraform/account-request.tf
+++ terraform/account-request.tf
@@ -1,1 +1,33 @@
+module "aft_account_import" {
+  source = "./modules/aft-account-request"
 
+  control_tower_parameters = {
+    AccountEmail = "ramdonti+lgasandbox-test@amazon.com"
+    AccountName  = "lgasandbox"
+    # Syntax for top-level OU
+    ManagedOrganizationalUnit = "DistrictNutrition"
+    # Syntax for nested OU
+    # ManagedOrganizationalUnit = "DistrictNutrition ou-p1uj-lbglh4tx"
+    SSOUserEmail     = "ramdonti+lgasandbox@amazon.com"
+    SSOUserFirstName = "ramesh"
+    SSOUserLastName  = "donti"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "ramdonti+lgasandbox@amazon.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Ramesh Babu"
+    change_reason       = "testing the account vending process"
+  }
+
+  account_customizations_name = "SANDBOX"
+}
